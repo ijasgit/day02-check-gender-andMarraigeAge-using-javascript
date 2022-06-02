8e96d6fb90302b42b1e006074bcb474908695614
Ijasgit <mohammedijasn3899@gmail.com>
2022-06-02 15:24:02 +0530

    marraige-eligiblity.

---
+++ check-gender-marriageAge.js
@@ -0,0 +1,24 @@
+let age,gen;
+gen="male";
+//gen="female";
+age=20;
+if(gen=="male"){
+    if(age>=21)
+    {
+        console.log("Eligible for marraige");
+    }
+    else{
+        console.log("Not Eligible for marraige");
+
+    }
+}
+else if(gen=="female"){
+    if(age>=18)
+    {
+        console.log("Eligible for marraige");
+    }
+    else{
+        console.log("Not Eligible for marraige");
+
+    }
+}
